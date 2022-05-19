02d18abb5c28ec7b12cb406ad0c2a89ca58f2dd7
Shakhzod <orinboevshahzod20@gmail.com>
2022-05-19 11:16:44 +0500

    typescript

---
+++ src/App3.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { User } from "./components/state/User";
+
+function App3() {
+  return (
+    <div className="App">
+      <User />
+    </div>
+  );
+}
+
+export default App3;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App2 from "./App2";
+import App3 from "./App3";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>
 );
